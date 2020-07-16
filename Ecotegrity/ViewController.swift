09312ae83c52b8c94a2a7f2b2c5925282cdae7d5
Jolie Ip <ipjolie1507@gmail.com>
2020-07-15 17:26:38 -0700

    changed the image in the log in page

--- Ecotegrity/ViewController.swift
+++ Ecotegrity/ViewController.swift
@@ -8,13 +8,24 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
 
+    @IBOutlet weak var password: UITextField!
+    @IBOutlet weak var email: UITextField!
+    @IBOutlet weak var loginbutton: UIButton!
+    
+    @IBOutlet weak var logo: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
-    }
+       
+        }
+  
+   }
 
 
-}
+
+
+
 
