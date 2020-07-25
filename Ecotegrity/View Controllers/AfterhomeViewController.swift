c38832ac105a506c210261f28b475d3e610c1bf6
Tasha <tashaip77@gmail.com>
2020-07-25 00:01:41 -0700

    started the picked interest page

---
+++ Ecotegrity/View Controllers/AfterhomeViewController.swift
@@ -0,0 +1,11 @@
+//
+//  AfterhomeViewController.swift
+//  Ecotegrity
+//
+//  Created by Tasha Ip Ying Ler on 7/24/20.
+//  Copyright © 2020 ecotegrity. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
