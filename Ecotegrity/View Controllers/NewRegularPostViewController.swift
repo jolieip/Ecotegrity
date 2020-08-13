bcaeccb874e1b499a7d47571cee8c403a12ff962
Ishika Prashar <iprashar@berkeley.edu>
2020-08-13 16:09:45 -0700

    more general additions, post something section

---
+++ Ecotegrity/View Controllers/NewRegularPostViewController.swift
@@ -0,0 +1,30 @@
+//
+//  NewRegularPostViewController.swift
+//  Ecotegrity
+//
+//  Created by Ishika Prashar on 8/13/20.
+//  Copyright © 2020 ecotegrity. All rights reserved.
+//
+
+import UIKit
+
+class NewRegularPostViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
---
+++ Ecotegrity/View Controllers/PostingViewController.swift
@@ -0,0 +1,32 @@
+//
+//  PostingViewController.swift
+//  Ecotegrity
+//
+//  Created by Ishika Prashar on 8/12/20.
+//  Copyright © 2020 ecotegrity. All rights reserved.
+//
+
+import UIKit
+
+class PostingViewController: UIViewController {
+
+    @IBOutlet weak var howToPost: UIButton!
+    @IBOutlet weak var regularPost: UIButton!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
